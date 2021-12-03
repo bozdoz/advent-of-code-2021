1db360a45b862af8a6eafbdcf3a387ce31c82767
Benjamin DeLong <Benjamin.DeLong@globalrelay.net>
2021-12-03 16:48:18 -0400

    day 3! 💯 kind of awful; binaries; transposing; filtering arrays

---
+++ 03/three_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+
+	"bozdoz.com/aoc-2021/utils"
+)
+
+func TestExampleOne(t *testing.T) {
+	expected := 198
+	vals := utils.LoadFile("example.txt")
+	val, err := PartOne(vals)
+
+	if err != nil {
+		t.Log("error should be nil", err)
+		t.Fail()
+	}
+
+	if val != expected {
+		t.Logf("Answer should be %d, but got %d", expected, val)
+		t.Fail()
+	}
+}
+
+func TestExampleTwo(t *testing.T) {
+	expected := 230
+	vals := utils.LoadFile("example.txt")
+	val, err := PartTwo(vals)
+
+	if err != nil {
+		t.Log("error should be nil", err)
+		t.Fail()
+	}
+
+	if val != expected {
+		t.Logf("Answer should be %d, but got %d", expected, val)
+		t.Fail()
+	}
+}
