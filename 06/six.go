9e07b42fbe008b87fb41efd26b05e77b8e1253be
bozdoz <howaboutben@gmail.com>
2021-12-06 16:31:29 +0000

    Day 6! 🧨 ran out of memory; had to refactor

---
+++ 06/six.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"bozdoz.com/aoc-2021/utils"
+)
+
+// different puzzles require different file loaders
+var FileLoader = utils.LoadAsString
+
+// map of counter -> count
+type State [9]int
+
+func loadState(data string) (state State, err error) {
+	// get first line
+	lines := strings.Split(data, "\n")
+	vals := strings.Split(lines[0], ",")
+
+	for _, val := range vals {
+		i, err := strconv.Atoi(val)
+
+		if err != nil {
+			return state, err
+		}
+
+		state[i]++
+	}
+
+	return
+}
+
+// incrementDay decrements fish counters
+func incrementDay(state *State) {
+	populating := state[0]
+
+	// moves all counters to previous index
+	for i, val := range state[1:] {
+		state[i] = val
+	}
+
+	// 0-day counters are both moved to 8 and copied to 6
+	state[8] = populating
+	state[6] += populating
+}
+
+func fastforward(state *State, days int) {
+	next := 0
+	for next < days {
+		incrementDay(state)
+		next++
+	}
+}
+
+func PartOne(content string) (output int, err error) {
+	state, err := loadState(content)
+
+	fastforward(&state, 80)
+
+	// TODO: can't use utils.Sum because [9]int is not []int!!?
+	for _, val := range state {
+		output += val
+	}
+
+	return
+}
+
+func PartTwo(content string) (output int, err error) {
+	state, err := loadState(content)
+
+	fastforward(&state, 256)
+
+	// TODO: can't use utils.Sum because [9]int is not []int!!?
+	for _, val := range state {
+		output += val
+	}
+
+	return
+}
+
+func main() {
+	// safe to assume
+	filename := "input.txt"
+
+	data := FileLoader(filename)
+
+	answer, err := PartOne(data)
+
+	if err != nil {
+		fmt.Println("failed to parse PartOne", err)
+		return
+	}
+
+	fmt.Printf("Part One: %d \n", answer)
+
+	answer2, err := PartTwo(data)
+
+	if err != nil {
+		fmt.Println("failed to parse PartTwo", err)
+		return
+	}
+
+	fmt.Printf("Part Two: %d \n", answer2)
+}
---
+++ 06/six_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"testing"
+)
+
+// fill in the answers for each part (as they come)
+var answers = map[int]int{
+	1: 5934,
+	2: 26984457539,
+}
+
+func TestExampleOne(t *testing.T) {
+	expected, ok := answers[1]
+
+	if !ok {
+		return
+	}
+
+	vals := FileLoader("example.txt")
+	val, err := PartOne(vals)
+
+	if err != nil {
+		t.Log("error should be nil", err)
+		t.Fail()
+	}
+
+	if val != expected {
+		t.Logf("Answer should be %d, but got %d", expected, val)
+		t.Fail()
+	}
+}
+
+func TestExampleTwo(t *testing.T) {
+	expected, ok := answers[2]
+
+	if !ok {
+		return
+	}
+
+	vals := FileLoader("example.txt")
+	val, err := PartTwo(vals)
+
+	if err != nil {
+		t.Log("error should be nil", err)
+		t.Fail()
+	}
+
+	if val != expected {
+		t.Logf("Answer should be %d, but got %d", expected, val)
+		t.Fail()
+	}
+}
