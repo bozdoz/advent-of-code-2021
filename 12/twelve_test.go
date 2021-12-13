c7753ed54c4b6e11b6ef55b0988400434a2ee151
bozdoz <howaboutben@gmail.com>
2021-12-13 04:49:14 +0000

    not sure where I'm going wrong day 12

---
+++ 12/twelve_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"testing"
+)
+
+// fill in the answers for each part (as they come)
+var answers = map[int]int{
+	1: 10,
+	// 2: 1134,
+}
+
+var vals = FileLoader("example.txt")
+
+func TestExampleOne(t *testing.T) {
+	expected, ok := answers[1]
+
+	if !ok {
+		return
+	}
+
+	val, err := PartOne(vals)
+
+	if err != nil {
+		t.Log("error should be nil", err)
+		t.Fail()
+	}
+
+	if val != expected {
+		t.Logf("Answer should be %d, but got %d", expected, val)
+		t.Fail()
+	}
+}
+
+func TestExampleTwo(t *testing.T) {
+	expected, ok := answers[2]
+
+	if !ok {
+		return
+	}
+
+	val, err := PartTwo(vals)
+
+	if err != nil {
+		t.Log("error should be nil", err)
+		t.Fail()
+	}
+
+	if val != expected {
+		t.Logf("Answer should be %d, but got %d", expected, val)
+		t.Fail()
+	}
+}
