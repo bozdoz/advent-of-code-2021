8e862ef1e6151b549302f20b971e8b0efbdb9275
bozdoz <howaboutben@gmail.com>
2022-01-04 01:01:51 +0000

    Day 19 finished! 😰

--- 19/nineteen.go
+++ 19/nineteen.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"time"
 
+	"github.com/bozdoz/advent-of-code-2021/19/scanner3d"
 	"github.com/bozdoz/advent-of-code-2021/utils"
 )
 
@@ -19,11 +22,33 @@ func init() {
 }
 
 func PartOne(content []string) (output int, err error) {
+	scanner, _, err := scanner3d.MergeScanners(content)
+
+	output = len(scanner.Beacons)
+
 	return
 }
 
 func PartTwo(content []string) (output int, err error) {
-	return
+	_, positions, err := scanner3d.MergeScanners(content)
+
+	// get greatest manhattan distance
+	maxDistance := 0
+
+	for i, a := range positions {
+		for j, b := range positions {
+			if i == j {
+				continue
+			}
+			manhattan := scanner3d.ManhattanDistance(a, b)
+
+			if manhattan > maxDistance {
+				maxDistance = manhattan
+			}
+		}
+	}
+
+	return maxDistance, err
 }
 
 func main() {
@@ -32,21 +57,33 @@ func main() {
 
 	data := FileLoader(filename)
 
-	answer, err := PartOne(data)
+	partFlag := flag.Int("part", -1, "pass a flag for -part")
 
-	if err != nil {
-		fmt.Println("failed to parse PartOne", err)
-		return
+	flag.Parse()
+
+	if *partFlag < 2 {
+		start := time.Now()
+		answer, err := PartOne(data)
+
+		if err != nil {
+			fmt.Println("failed to parse PartOne", err)
+			return
+		}
+
+		fmt.Printf("Part One: %d \n", answer)
+		fmt.Printf("Time: %s \n", time.Since(start))
 	}
 
-	fmt.Printf("Part One: %d \n", answer)
+	if *partFlag != 1 {
+		start := time.Now()
+		answer2, err := PartTwo(data)
 
-	answer2, err := PartTwo(data)
+		if err != nil {
+			fmt.Println("failed to parse PartTwo", err)
+			return
+		}
 
-	if err != nil {
-		fmt.Println("failed to parse PartTwo", err)
-		return
+		fmt.Printf("Part Two: %d \n", answer2)
+		fmt.Printf("Time: %s \n", time.Since(start))
 	}
-
-	fmt.Printf("Part Two: %d \n", answer2)
 }
